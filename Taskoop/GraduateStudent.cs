10687dd6450cde8db3ea029bd48fdc15186d21aa
Ahmadal26 <Almusallam296@hotmail.com>
2024-04-15 10:44:21 +0300

    Add project files.

---
+++ Taskoop/GraduateStudent.cs
@@ -0,0 +1,29 @@
+﻿
+//Task1
+public class GraduateStudent: Student
+{
+
+    public bool IsEmpolyee { get; set; }
+
+    public GraduateStudent(string name, string major, double gpa,bool isEmpolyee):base (name, major, gpa) 
+    {
+        IsEmpolyee = isEmpolyee;
+
+    }
+    public override void PrintStudntInfo()
+    {
+        base.PrintStudntInfo();
+        Console.WriteLine($"is empolyee{IsEmpolyee}");
+    }
+}
+
+public class Task
+{
+    public static void Main() {
+        Student student = new Student("Ahmad", "Computer Eng", 3.38);
+        student.PrintStudntInfo ();
+        Console.WriteLine();
+        GraduateStudent graduateStudent = new GraduateStudent("Ahmad", "Computer Eng", 3.38,true);
+        graduateStudent.PrintStudntInfo ();
+} 
+}
---
+++ Taskoop/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Task1
+public class Student {
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public double GPA { get; set; }
+
+
+    public Student(string name,string major, double gpa)
+    {
+        Name = name;
+        Major = major;
+        GPA = gpa;
+
+
+    }
+
+    public virtual void PrintStudntInfo() {
+        Console.WriteLine($"Student Name: {Name}");
+        Console.WriteLine($"Major: {Major}");
+        Console.WriteLine($"GPA: {GPA}");
+        
+    }
+
+}
+
